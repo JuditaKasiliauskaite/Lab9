913b9b22a6eb56c323705d69895d7b86b7cf71e2
JUDITA KASILIAUSKAITE - STUDENT <G00348535@gmit.ie>
2019-11-11 11:52:20 +0000

    first commit

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import {Router, ActivatedRoute} from '@angular/router';
+import {MovieServiceService} from '../Services/movie-service.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-
-  constructor() { }
+  movie:any=[];
+  constructor(private movieService: MovieServiceService, private router:Router, private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.movieService.GetMovie(this.route.snapshot.params['id']).subscribe((data) => {
+      this.movie = data;
+      console.log(this.movie);
+    })
+
+  }
+
+  onEditMovie(form: NgForm){
+    this.movieService.UpdateMovie(this.movie._id, form.value.title, form.value.year, form.value.poster);
   }
 
 }
